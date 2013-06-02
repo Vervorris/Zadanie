dc449646e229d240870f72d23befb2031f4a8e46
Patryk Perduta <vervorris@gmail.com>
2013-06-02 11:36:16 +0200

    zdeklarowalem liste

--- like.php
+++ like.php
@@ -1,5 +1,14 @@
 <?php
 //deklaracja listy fanpage'y
+$fanpage = array(
+	"woblink" => 0,
+	"legimi" => 0,
+	"wolnelektury" => 0,
+	"RW2010pl" => 0,
+	"Virtualo" => 0,
+	"bookoteka" => 0,
+	"bookrage" => 0
+);
 
 //sprawdzanie listy
 
